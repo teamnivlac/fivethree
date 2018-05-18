8383ad2428e5cb2a2e995ff497e576894572fb03
Calvin Burkhead <calvin.nivlac@gmail.com>
2018-05-17 19:24:44 -0700

    It works

--- src/main/java/com/teamnivlac/fivethree/controllers/DefaultController.java
+++ src/main/java/com/teamnivlac/fivethree/controllers/DefaultController.java
@@ -4,9 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Properties;
 
 import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
-import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.route53.AmazonRoute53;
 import com.amazonaws.services.route53.AmazonRoute53ClientBuilder;
@@ -29,21 +29,22 @@ public class DefaultController {
 		String ip = in.readLine(); // you get the IP as a String
 		System.out.println(ip);
 
-		String hostedZoneID = "Z3H0D0Y3KELQ5O";
-		String domain = "hq.teamnivlac.com";
+		Properties prop = new Properties();
+		prop.load(DefaultController.class.getResourceAsStream("/settings/settings.properties"));
+		
+		
+		String hostedZoneID = prop.getProperty("hostedZoneID");
+		String domain = prop.getProperty("domain");
 
 		AmazonRoute53 client = AmazonRoute53ClientBuilder.standard().withRegion(Regions.US_WEST_2).build();
 		ChangeResourceRecordSetsRequest request = new ChangeResourceRecordSetsRequest().withHostedZoneId(hostedZoneID)
 				.withChangeBatch(new ChangeBatch().withComment("Web server for example.com").withChanges(
 						new Change().withAction("UPSERT").withResourceRecordSet(new ResourceRecordSet().withName(domain)
-								.withType("A").withTTL(300L).withResourceRecords(new ResourceRecord().withValue(ip)))));
+								.withType("A").withTTL(299L).withResourceRecords(new ResourceRecord().withValue(ip)))));
 		request.withRequestCredentialsProvider(
 				new ClasspathPropertiesFileCredentialsProvider("/credentials/credentials.properties"));
 		System.out.println("Sending request");
 		ChangeResourceRecordSetsResult response = client.changeResourceRecordSets(request);
 		System.out.println(response.toString());
 	}
-
-	// Connect to AWS
-
 }
